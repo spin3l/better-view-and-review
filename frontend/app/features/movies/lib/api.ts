6b9660aaa690c4d0d4e027c3804e789212742dc2
spin3l <dev.christianlopezgarcia@gmail.com>
2025-06-13 13:28:02 +0200

    chore: fixed node version

--- frontend/app/features/movies/lib/api.ts
+++ frontend/app/features/movies/lib/api.ts
@@ -3,8 +3,6 @@ import type { Movie } from "@/features/movies/types/movie";
 
 const api_key = process.env.TMDB_API_KEY;
 
-console.log(api_key);
-
 const TMDB_API = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   headers: {
